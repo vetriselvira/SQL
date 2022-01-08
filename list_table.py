f4e5fecbc6fcb61fafddfbdbb0f297f0ebc60581
vetriselvira <vetriselvira@gmail.com>
2022-01-08 13:37:55 -0500

    New Changes

--- list_table.py
+++ list_table.py
@@ -1,10 +1,22 @@
 import sqlite3
 conn = sqlite3.connect("vetri.db")
 cursor = conn.cursor()
-conn.executescript("DROP TABLE vetri;CREATE TABLE vetri(id,age,name);")
-cursor.execute("INSERT INTO vetri VALUES(1,32,'rajangam');")
-select_all = "SELECT * FROM vetri;"
+conn.executescript("CREATE TABLE IF NOT EXISTS 'outlet'(outlet_id INT PRIMARY KEY,outlet_name VARCHAR(100),outlet_address VARCHAR(100));")
+outlet_id = input("Enter outlet_id:")
+outlet_name = input("Enter outlet_name:  ")
+oulet_address = input("Enter outlet address:")
+sql = "insert into outlet(outlet_id,outlet_name,outlet_address) values(?,?,?)"
+val = (outlet_id,outlet_name,oulet_address)
+#cursor.execute("INSERT INTO outlet VALUES(1,'Main_outlet','Rt:9,Oldbridge');")
+cursor.execute(sql,val)
+select_all = "SELECT * FROM outlet;"
 cursor.execute(select_all)
 print(cursor.fetchall())
 conn.commit()
 conn.close()
+
+
+
+#sql = "insert into emp(id, name, salary) values(%d, %s, %s)"
+
+#val = (id, name, salary)
